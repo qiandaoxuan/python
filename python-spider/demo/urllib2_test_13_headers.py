5b2964f963f82ac92aa24ffdfafd109e88ce87ac
fengru.ji <fengru.ji@arcserve.com>
2016-11-08 14:24:22 +0800

    118

--- python-spider/demo/urllib2_test_13_headers.py
+++ python-spider/demo/urllib2_test_13_headers.py
@@ -2,5 +2,5 @@ import urllib2
 
 hds = {
     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
-    'Referer': 'http://www.cnbeta.com/articles' #f反盗链
+    'Referer': 'http://www.cnbeta.com/articles' #反盗链
 }
